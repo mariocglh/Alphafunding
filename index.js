6a43ef81079dd080d8a27ba3fd9d39310cfe3a3d
MarioCEO <mariocglh@gmail.com>
2026-01-15 19:39:11 +0100

    Renombrado final a godMode.js para evitar error de windows

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const helmet = require('helmet');
 const prisma = require('./src/config/db');
-const adminRoutes = require('./routes/adminRoutesFinal');
+const adminRoutes = require('./routes/godmode');
 // --- RUTAS MODULARES ---
 const authRoutes = require('./src/routes/authRoutes');
 const tradeRoutes = require('./src/routes/tradeRoutes');
